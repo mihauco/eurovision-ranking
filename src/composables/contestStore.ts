b41057cd3624341a9e5f4ae01f3e2be923793596
mihauco <github@mihau.co>
2024-05-10 15:22:09 +0200

    improve data storage

--- src/composables/contestStore.ts
+++ src/composables/contestStore.ts
@@ -1,12 +1,31 @@
 import { ref, watch } from "vue";
 import type { ContestData } from "@/types/ContestData.type";
 import defaultContestData from "@/data/2024.json";
+import type { SongData } from "@/types/SongData.type";
 
 const contestData = (() => {
+  const localStorageRankingData = localStorage.getItem("rankingData");
+  
+
+  if (localStorageRankingData) {
+    const rankingData = JSON.parse(localStorageRankingData);
+
+    const final = rankingData.map((country_code: string) => {
+      return defaultContestData.final.find((song) => song.country_code === country_code)
+    }).filter((song: any) => song !== undefined)
+
+    return ref<ContestData>({ final })
+  }
+
+  // Just in case someone has the old localStorage data
   const localStorageDataJson = localStorage.getItem("contestData");
 
   if (localStorageDataJson) {
-    return ref<ContestData>(JSON.parse(localStorageDataJson))
+    return ref<ContestData>({
+      final: JSON.parse(localStorageDataJson).final.map(({country_code}: SongData) => {
+        return defaultContestData.final.find((song) => song.country_code === country_code)
+      }).filter((song: any) => song !== undefined)
+    })
   }
 
   return ref<ContestData>(defaultContestData)}
@@ -15,7 +34,9 @@ const contestData = (() => {
 watch(
   contestData,
   (newData) => {
-    localStorage.setItem("contestData", JSON.stringify(newData))
+    localStorage.setItem("rankingData", JSON.stringify({
+      final: newData.final.map(song => song.country_code)
+    }))
   },
   { deep: true }
 )
